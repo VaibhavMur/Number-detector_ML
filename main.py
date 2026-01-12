8c43a34e30fc7af99df0d7dedb2a7a08cac89766
VaibhavMurali <z5589018@ad.unsw.edu.au>
2026-01-12 15:14:48 +1100

    Load model from train.py and test with human handwriting

---
+++ main.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import cv2
+import tensorflow as tf
+import numpy as np
+
+model = tf.keras.models.load_model('handwritten_model.keras') # Loading the model
+
+img = cv2.imread('Opera Snapshot_2026-01-12_144050_opera.png', cv2.IMREAD_GRAYSCALE)
+img = cv2.resize(img, (28, 28))
+img = np.expand_dims(img, axis=0)
+
+prediction = model.predict(img)
+print (f"Number: {np.argmax(prediction)}")
