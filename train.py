3919cec8b4c20354b5ee023e04f4a78bfb5975cb
VaibhavMurali <z5589018@ad.unsw.edu.au>
2026-01-12 15:11:09 +1100

    Extracted MNIST dataset and normalized images to the [0,1] range

---
+++ train.py
@@ -0,0 +1,11 @@
+import os
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Normalize the images to [0, 1] range
+x_train = tf.keras.utils.normalize(x_train, axis = 1)
+x_test = tf.keras.utils.normalize(x_test, axis = 1)
