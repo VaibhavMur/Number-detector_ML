34c71f209670096024a869b93c28b7de211c7624
VaibhavMurali <z5589018@ad.unsw.edu.au>
2026-01-12 15:12:54 +1100

    Called sequential model, compressed 2D images to 1D and then added 3 layers of neural network with softmax showing confidence

--- train.py
+++ train.py
@@ -9,3 +9,9 @@ mnist = tf.keras.datasets.mnist
 # Normalize the images to [0, 1] range
 x_train = tf.keras.utils.normalize(x_train, axis = 1)
 x_test = tf.keras.utils.normalize(x_test, axis = 1)
+
+model = tf.keras.models.Sequential() # Calling the Sequential model
+model.add(tf.keras.layers.Flatten(input_shape = (28, 28))) # Flattening the 2D images to 1D vectors
+model.add(tf.keras.layers.Dense(128, activation = 'relu')) # First hidden layer with ReLU activation. RELU stands for Rectified Linear Unit
+model.add(tf.keras.layers.Dense(128, activation = 'relu')) # Second hidden layer with ReLU activation. RELU stands for Rectified Linear Unit
+model.add(tf.keras.layers.Dense(10, activation = 'softmax')) # Output layer with Softmax activation. Softmax makes sure the outputs add up to 1, so they can be interpreted as probabilities/confidence
